a415297fc5fface3282807f96fa403950afa1816
hesam <mofidihesam8@gmail.com>
2024-03-04 09:37:33 +0330

    SystemPermissionFilterSearch

--- Application/Features/SystemFeatures/Commands/SystemRequestsHandlersCommand.cs
+++ Application/Features/SystemFeatures/Commands/SystemRequestsHandlersCommand.cs
@@ -51,5 +51,67 @@ namespace Application.Features.SystemFeatures.Commands
             }
         }
         #endregion
+
+        #region EditSystem
+        public class EditSystemRequestCommand : IRequest<BaseCommandResponse>
+        {
+            public SystemInfoDto? systemDto { get; set; }
+        }
+        public class EditSystemHandlerCommand : IRequestHandler<EditSystemRequestCommand, BaseCommandResponse>
+        {
+            private readonly IMapper _mapper;
+            private readonly ISystemsRepository _systemsRepository;
+            public EditSystemHandlerCommand(IMapper mapper, ISystemsRepository systemsRepository)
+            {
+                _mapper = mapper;
+                _systemsRepository = systemsRepository;
+            }
+            public async Task<BaseCommandResponse> Handle(EditSystemRequestCommand request, CancellationToken cancellationToken)
+            {
+                var response = new BaseCommandResponse();
+                var validator = new SystemValidator(_systemsRepository);
+                var validationResult = await validator.ValidateAsync(request.systemDto);
+                if (validationResult.IsValid == false)
+                {
+                    response.Success = false;
+                    response.Message = "Update Failed";
+                    response.Errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+                }
+                else
+                {
+                    //var systemInfo = _mapper.Map<Systems>(request.systemDto);
+                    //await _systemsRepository.AddAsync(systemInfo);
+                    var entity = await _systemsRepository.GetByIdAsync(request.systemDto.Id);
+                    _mapper.Map(request.systemDto ,entity);
+                    await _systemsRepository.UpdateAsync(entity);
+                    response.Success = true;
+                    response.Message = "Update Successful";
+                }
+                return response;
+            }
+        }
+        #endregion
+
+        #region DeleteSystem
+        public class DeleteSystemRequestCommand : IRequest
+        {
+            public int Id { get; set; }
+        }
+        public class DeleteSystemHandlerCommand : IRequestHandler<DeleteSystemRequestCommand>
+        {
+            private readonly ISystemsRepository _systemsRepository;
+            public DeleteSystemHandlerCommand(IMapper mapper, ISystemsRepository systemsRepository)
+            {
+                _systemsRepository = systemsRepository;
+            }
+
+            public async Task Handle(DeleteSystemRequestCommand request, CancellationToken cancellationToken)
+            {
+                var entity = await _systemsRepository.GetByIdAsync(request.Id);
+                await _systemsRepository.DeleteAsync(entity);
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Application/Features/SystemFeatures/Queries/GetSystemsRequestHandlerQuery.cs
@@ -0,0 +1,85 @@
+﻿using Application.Contract.Persistance.SystemsRolesManagment;
+using Application.Dtos.CommonDtos;
+using Application.Dtos.PermissionsDtos;
+using Application.Dtos.SystemsDto;
+using Application.Models.Abstraction;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.SystemFeatures.Queries
+{
+    public class GetSystemsRequestHandlerQuery
+    {
+        #region FilterandSerch
+        public class GetSystemsFilterRequestQuery : IRequest<PagedList<SystemInfoDto>>
+        {
+            public FilterDataDto? FilterDataDto { get; set; }
+        }
+        public class GetSystemsFilterHandlerQuery : IRequestHandler<GetSystemsFilterRequestQuery, PagedList<SystemInfoDto>>
+        {
+            private readonly IMapper _mapper;
+            private readonly ISystemsRepository _systemsRepository;
+            public GetSystemsFilterHandlerQuery(IMapper mapper, ISystemsRepository systemsRepository)
+            {
+                _mapper = mapper;
+                _systemsRepository = systemsRepository;
+            }
+            public async Task<PagedList<SystemInfoDto>> Handle(GetSystemsFilterRequestQuery request, CancellationToken cancellationToken)
+            {
+                var filterdata = _mapper.Map<FilterData>(request.FilterDataDto);
+                var systems = await _systemsRepository.FilterAsync(filterdata);
+                var systemItems = _mapper.Map<List<SystemInfoDto>>(systems.Items);
+                return new PagedList<SystemInfoDto>(systemItems, systems.Paging.PageSize, systems.Paging.CurrentPage, systems.Paging.TotalRecordCount);
+            }
+        }
+
+        public class GetSystemsSearchRequestQuery : IRequest<PagedList<SystemInfoDto>>
+        {
+            public SearchDataDto? searchDataDto { get; set; }
+        }
+        public class GetSystemsSearchHandlerQuery : IRequestHandler<GetSystemsSearchRequestQuery, PagedList<SystemInfoDto>>
+        {
+            private readonly IMapper _mapper;
+            private readonly ISystemsRepository _systemsRepository;
+            public GetSystemsSearchHandlerQuery(IMapper mapper, ISystemsRepository systemsRepository)
+            {
+                _mapper = mapper;
+                _systemsRepository = systemsRepository;
+            }
+            public async Task<PagedList<SystemInfoDto>> Handle(GetSystemsSearchRequestQuery request, CancellationToken cancellationToken)
+            {
+                var searchData = _mapper.Map<SearchData>(request.searchDataDto);
+                var systems = await _systemsRepository.SearchAsync(searchData);
+                var systemItems = _mapper.Map<List<SystemInfoDto>>(systems.Items);
+                return new PagedList<SystemInfoDto>(systemItems, systems.Paging.PageSize, systems.Paging.CurrentPage, systems.Paging.TotalRecordCount);
+
+            }
+        }
+        #endregion
+
+        #region GetById
+        public class GetSystemsRequestQuery : IRequest<SystemInfoDto>
+        {
+            public int systemId { get; set; }
+        }
+        public class GetSystemsHandlerQuery : IRequestHandler<GetSystemsRequestQuery, SystemInfoDto>
+        {
+            private readonly IMapper _mapper;
+            private readonly ISystemsRepository _systemsRepository;
+
+            public GetSystemsHandlerQuery(ISystemsRepository systemsRepository, IMapper mapper)
+            {
+                _systemsRepository = systemsRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<SystemInfoDto> Handle(GetSystemsRequestQuery request, CancellationToken cancellationToken)
+            {
+                var entity = await _systemsRepository.GetByIdAsync(request.systemId);
+                var system = _mapper.Map<SystemInfoDto>(entity);
+                return system;
+            }
+        }
+        #endregion
+    }
+}
